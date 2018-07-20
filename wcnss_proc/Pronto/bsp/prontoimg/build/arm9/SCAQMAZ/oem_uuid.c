e0393e7c409e01f1348bf3c8d390fa86de3ebe65
sahthi <umarani.7288@gmail.com>
2018-07-20 17:35:11 +0530

    added files

---
+++ wcnss_proc/Pronto/bsp/prontoimg/build/arm9/SCAQMAZ/oem_uuid.c
@@ -0,0 +1,17 @@
+/*
+#============================================================================
+#  Name:                                                                     
+#    oem_uuid.c 
+#
+#  Description:                                                              
+#    None 
+#                                                                            
+# Copyright (c) 2017 by QUALCOMM, Incorporated.  All Rights Reserved.        
+#============================================================================
+#                                                                            
+# *** AUTO GENERATED FILE - DO NOT EDIT                                      
+#                                                                            
+# GENERATED: Thu Apr 13 22:39:04 2017 
+#============================================================================
+*/
+const char OEM_IMAGE_UUID_STRING_AUTO_UPDATED[]="OEM_IMAGE_UUID_STRING=Q_SENTINEL_{65E535C5-C8E4-45D3-B65D-5BCBA167806B}_20170413_2239";
